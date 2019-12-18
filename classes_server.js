6ca7b3535453f62427258c2f0b41643d7ca15f73
Cloud Project <cloudprojects4@gmail.com>
2019-12-18 22:46:44 +0530

    first commit

---
+++ classes_server.js
@@ -0,0 +1,43 @@
+var express=require('express');
+var bodyParser=require('body-parser');
+var http=require('http');
+
+var app=express();
+app.use(bodyParser.json()); // Accept JSON params
+app.use(bodyParser({limit:'50mb'}));// for sending large payload size
+app.use(bodyParser({extended: true}));
+app.use(bodyParser.urlencoded({extended:true}))// Accept URL Encoded params
+
+
+// for accessing image folder globally.
+app.use(express.static('public'));
+//Serves all the request which includes /upload in the url from Images folder
+app.use('/upload', express.static(__dirname + '/profile_upload'));
+app.use('/upload', express.static(__dirname + '/doc_upload'));
+
+
+var server=http.createServer( function(req,res){
+    res.setHeader('Content-Type', 'application/json');
+    //res.writable(sat)
+
+});
+
+ server = app.listen(6000, function () {
+    var host = server.address().address
+    var port = server.address().port
+    
+    console.log("Server app listening at http://%s:%s", host, port)
+ });
+
+
+
+
+ 
+ // handling routes
+ // just for example
+  var user_login=require('./routes/login');
+
+  app.post('/login',user_login.userLogin);
+  
+
+
---
+++ config/dbconnect.js
@@ -0,0 +1,32 @@
+var mysql = require('mysql');
+
+// local setup 
+var pool = mysql.createPool({
+    host:'localhost',
+    port:'3306',
+    user:'root',
+    password:'fundroid',
+    database:'classes'
+});
+
+// global setup
+// var pool = mysql.createPool({
+//   host:'localhost',
+//   port:'3306',
+//   user:'fundrod',
+//   password:'Fundroid@info4163',
+//   database:'classes'
+// });
+
+
+var getConnection = function (cb) {
+    pool.getConnection(function (err, connection) {
+        //if(err) throw err;
+        //pass the error to the cb instead of throwing it
+        if(err) {
+          return cb(err);
+        }
+        cb(null, connection);
+    });
+};
+module.exports = getConnection;
